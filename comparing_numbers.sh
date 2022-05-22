7cc20bbeb956160a8369ddbfd114ce6531c0e2ab
kenechukwu nzute <kenechukwunzute@gmail.com>
2022-05-22 08:24:51 +0000

    Added Comparing Integer Numbers script

---
+++ comparing_numbers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+echo "Enter the first integer number:"
+read X
+
+echo "Enter the second integer number:"
+read Y
+
+if [ $X -lt $Y ]
+then
+	    echo "$X is less than $Y"
+    elif [ $X -gt $Y ]
+    then
+	        echo "$X is greater than $Y"
+	else
+		    echo "$X is equal to $Y"
+fi
